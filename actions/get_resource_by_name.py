48318d464df6f28e680d5edb64b9180ee9d7dcfc
Martez Reed <martez.reed@kinneygroup.com>
2017-02-10 07:09:24 -0600

    Add icon and updated get resource by name to provide additional detail

--- actions/get_resource_by_name.py
+++ actions/get_resource_by_name.py
@@ -3,5 +3,10 @@ import json
 
 class GetResourceByName(action.vRealizeAutomationAction):
     def run(self, resourceName):
+        resourceJSONString = ''
         resource = self.vra7.getResourceByName(name=resourceName)
-        return json.dumps(resource)
+        data = json.dumps(resource)
+        jsondata = json.loads(data)
+        resourceId = jsondata["id"]
+        resourceDetail = self.vra7.getResource(id=resourceId, show='json')
+        return json.dumps(resourceDetail)
